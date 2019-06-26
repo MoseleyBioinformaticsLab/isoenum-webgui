a093e7bcb460dd1657d95966aa462a8ddc11080d
andreysmelter <andrey.smelter@gmail.com>
2019-06-26 00:01:20 -0400

    Add home page JS.

---
+++ isoenum_webgui/static/custom/js/home.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+
+    $('.custom-file-input').on('change', function(event) {
+        $(this).next('.custom-file-label').html(event.target.files[0].name);
+    });
+});
