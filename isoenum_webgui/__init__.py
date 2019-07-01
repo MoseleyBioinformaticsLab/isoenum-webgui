da3a99c4ead1bfd2393d53e83668b13431472cbc
andreysmelter <andrey.smelter@gmail.com>
2019-06-30 23:17:58 -0400

    Using header config file.

--- isoenum_webgui/__init__.py
+++ isoenum_webgui/__init__.py
@@ -7,27 +7,15 @@ import flask_excel
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'you-will-never-guess'
 
+RECORDS = {}
+NMR_DATA = {}
+
 this_dir = os.path.dirname(__file__)
 with open(os.path.join(this_dir, "config", "example_project.json"), "r") as inf:
     example_project = json.load(inf)
 
-RECORDS = {}
-NMR_DATA = {}
-HEADER = [
-    {"title": "Name", "editable": True, "hidden": False},
-    {"title": "Base Identifier", "editable": True, "hidden": False},
-    {"title": "Base SVG Str", "editable": False, "hidden": True},
-    {"title": "Base SVG", "editable": False, "hidden": False},
-    {"title": "Base Molfile", "editable": False, "hidden": True},
-    {"title": "ISO", "editable": True, "hidden": False},
-    {"title": "CHG", "editable": True, "hidden": False},
-    {"title": "Repr Identifier", "editable": False, "hidden": False},
-    {"title": "Repr SVG Str", "editable": False, "hidden": True},
-    {"title": "Repr SVG", "editable": False, "hidden": False},
-    {"title": "Repr Molfile", "editable": False, "hidden": True},
-    {"title": "record_id", "editable": False, "hidden": True},
-]
-
+with open(os.path.join(this_dir, "config", "header.json"), "r") as inf:
+    HEADER = json.load(inf)
 
 flask_excel.init_excel(app)
 
