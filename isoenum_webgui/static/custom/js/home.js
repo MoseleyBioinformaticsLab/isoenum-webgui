3fa40433537530837ae574984ca74233a048d5db
andreysmelter <andrey.smelter@gmail.com>
2019-07-02 18:39:51 -0400

    Update home page JS.

--- isoenum_webgui/static/custom/js/home.js
+++ isoenum_webgui/static/custom/js/home.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
 
-    $('.custom-file-input').on('change', function(event) {
-        $(this).next('.custom-file-label').html(event.target.files[0].name);
+    $(".custom-file-input").on("change", function(event) {
+        $(this).next(".custom-file-label").html(event.target.files[0].name);
     });
+
 });
