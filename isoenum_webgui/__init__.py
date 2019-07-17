ef1d7cb777ed5a7aeeb1f79f20f8a7fde68e30a0
andreysmelter <andrey.smelter@gmail.com>
2019-07-17 13:39:35 -0400

    Import custom errors and register errors blueprint.

--- isoenum_webgui/__init__.py
+++ isoenum_webgui/__init__.py
@@ -1,8 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Isotopic enumerator web interface.
+"""
+
 import json
 import os
 
 from flask import Flask
 import flask_excel
+
+from .errors import errors
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "you-will-never-guess"
@@ -17,6 +26,7 @@ with open(os.path.join(this_dir, "config", "example_project.json"), "r") as inf:
 with open(os.path.join(this_dir, "config", "header.json"), "r") as inf:
     HEADER = json.load(inf)
 
+app.register_blueprint(errors)
 flask_excel.init_excel(app)
 
 from isoenum_webgui import routes
