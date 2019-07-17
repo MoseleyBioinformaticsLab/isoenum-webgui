9a80c73049cd6da4f1204c97bad4b3a00ec29a14
andreysmelter <andrey.smelter@gmail.com>
2019-07-17 14:58:58 -0400

    Updated custom errors file.

--- isoenum_webgui/errors.py
+++ isoenum_webgui/errors.py
@@ -21,11 +21,8 @@ def handle_error(error):
     status_code = 500
     success = False
     response = {
-        'success': success,
-        'error': {
-            'type': error.__class__.__name__,
-            'message': message
-        }
+        "success": success,
+        "error": {"type": error.__class__.__name__, "message": message},
     }
 
     return jsonify(response), status_code
