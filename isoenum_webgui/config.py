7b40bd34fa890a17ee438e90ea8c0bba0e9d387b
andreysmelter <andrey.smelter@gmail.com>
2019-08-01 14:34:39 -0400

    Docs: module level docstring.

--- isoenum_webgui/config.py
+++ isoenum_webgui/config.py
@@ -1,6 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+isoenum_webgui.config
+~~~~~~~~~~~~~~~~~~~~~
+
+
+This module contains `isoenum-webgui` application configurations.
+"""
+
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
+    """Set `isoenum-webgui` Flask app configuration."""
+
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
