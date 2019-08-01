4db4dde275b1a36f7d9b5be203dc575e0884541a
andreysmelter <andrey.smelter@gmail.com>
2019-08-01 16:16:18 -0400

    Docs: updated module level docstring.

--- isoenum_webgui/forms.py
+++ isoenum_webgui/forms.py
@@ -2,6 +2,9 @@
 # -*- coding: utf-8 -*-
 
 """
+isoenum_webgui.forms
+~~~~~~~~~~~~~~~~~~~~
+
 Isotopic enumerator web interface forms.
 """
 
@@ -10,4 +13,6 @@ from flask_wtf.file import FileField, FileRequired
 
 
 class FileForm(FlaskForm):
+    """File input form with validation."""
+
     file = FileField(validators=[FileRequired()])
