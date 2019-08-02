3f9eb218468469e8cfb30fbdb9fb53b47d3772a2
andreysmelter <andrey.smelter@gmail.com>
2019-08-02 12:00:09 -0400

    Add new ENV config.

--- isoenum_webgui/config.py
+++ isoenum_webgui/config.py
@@ -10,10 +10,12 @@ This module contains `isoenum-webgui` application configurations.
 """
 
 import os
+
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
     """Set `isoenum-webgui` Flask app configuration."""
 
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SECRET_KEY = os.environ.get("SECRET_KEY") or "you-will-never-guess"
+    ENV = os.environ.get("FLASK_ENV") or "development"
