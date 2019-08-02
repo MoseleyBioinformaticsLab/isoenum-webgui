c3f81f7b371ad68df57d5f00e47a567e71a2fee0
andreysmelter <andrey.smelter@gmail.com>
2019-08-02 16:14:28 -0400

    Define entry_point.

--- setup.py
+++ setup.py
@@ -66,5 +66,6 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Topic :: Scientific/Engineering :: Bio-Informatics",
         "Topic :: Software Development :: Libraries :: Python Modules",
-    ]
+    ],
+    entry_points={"console_scripts": ["isoenum_webgui = isoenum_webgui.__main__:main"]},
 )
